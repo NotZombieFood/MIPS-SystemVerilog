4d4111e5d9a2adb55b4db93e3b457fb093fad21f
Gerardo Cruz <gerardocd1000@gmail.com>
2017-10-12 21:46:26 -0500

    MIPS IS WORKING

--- DataMemory.sv
+++ DataMemory.sv
@@ -1,15 +1,30 @@
 `timescale 1ns / 1ps
 
-module DataMemory(
-    input [31:0] Address,
-    input [31:0] WriteData,
-    input MemWrite,
-    input MemRead,
-	 input clk,
-    output logic [31:0] ReadData
-    );
+module DataMemory 
+#(
+  parameter NUM_REGS = 16, 
+  parameter SIZE = 32
+)(
+  input clk,
+  input rst,
+  input  [3:0]  wr_addr,
+  input         wr_en,
+  input  [31:0] wr_data,
+  input  [3:0]  rd_addr,
+  output [31:0] rd_data
+);
+  
+    logic [SIZE-1:0] rf [NUM_REGS-1:0];
+  
+    integer i;
+	 always_ff @ (posedge clk) begin
+      if (rst)
+			for (i = 0; i < NUM_REGS-1; i = i + 1)
+				rf[i] <= 0;
+		else if (wr_en) 
+        rf[wr_addr] <= wr_data;
+	end
 
-always_comb begin
-	ReadData= 32'b00000000000000000000000000000000;
-end
+    assign rd_data = rf[rd_addr];
+
 endmodule
